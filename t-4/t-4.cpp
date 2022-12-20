469eca45753b41d5d6c9550db8d41637e952028f
chocojuho <yhg3669002@gmail.com>
2022-12-20 11:32:45 +0900

    랜덤 기능구현

---
+++ t-4/t-4.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+char arr[4] = { '*','-','+','/' };
+int a, b;
+int ans;
+int main()
+{
+	srand(time(0));
+	char 부호=arr[rand() % 4];
+	a = rand();
+	b = rand();
+	cout << a << ' ' << 부호 << ' ' << b << ' ' << '=';
+	cin >> ans;
+	
+}
