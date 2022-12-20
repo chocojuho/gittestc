c258c1636a6fc8827808e08ad2ff25c19105611c
chocojuho <yhg3669002@gmail.com>
2022-12-20 11:39:25 +0900

    반복하기

--- t-4/t-4.cpp
+++ t-4/t-4.cpp
@@ -5,13 +5,18 @@ using namespace std;
 char arr[4] = { '*','-','+','/' };
 int a, b;
 int ans;
+int cnt;
 int main()
 {
-	srand(time(0));
-	char 부호=arr[rand() % 4];
-	a = rand();
-	b = rand();
-	cout << a << ' ' << 부호 << ' ' << b << ' ' << '=';
-	cin >> ans;
-	
+	while(true) {
+		if (cnt == 10) {
+			break;
+		}
+		srand(time(0));
+		char 부호 = arr[rand() % 4];
+		a = rand()%20 +1;
+		b = rand()%20 +1;
+		cout << '[' << cnt << ',' << 10 << ']' << ' ' << a << ' ' << 부호 << ' ' << b << ' ' << '=';
+		cin >> ans;
+	}
 }
